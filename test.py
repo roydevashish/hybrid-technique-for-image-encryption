08d312f826e4814d8b81073f1a1fd6560157fb64
Devashish Roy <medevashishroy@gmail.com>
2024-12-22 20:56:00 +0530

    modified
    Code for test script is modified to implement the image encryption process.

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from imgtohex import image_to_hex_matrix
+from hextoimg import hex_matrix_to_image
 from chacha20 import encryption, decryption
 
 from os import urandom
 
 if __name__ == "__main__":
-    input_img_path = "input/sample.jpg"
-    output_img_path = "output/sample.jpg"
+    input_img_path = "input/sample-01.jpg"
+    output_img_path = "output/sample-01.jpg"
 
     img_matrix = image_to_hex_matrix(input_img_path)
     key = "f81d2e2288bd230adaaab9e6c84aa7689550fe3d271df579bd9bb6e8a379e1d1"
@@ -17,8 +18,10 @@ if __name__ == "__main__":
 
     ciphertext, nonce = encryption(plaintext, key)
 
-    for i in range(0, len(ciphertext), 6):
-        print(ciphertext[i: i+6])
+    i = 0
+    for row in range(len(img_matrix)):
+        for col in range(len(img_matrix[row])):
+            img_matrix[row][col] = ciphertext[i:i+6]
+            i+=6
 
-
-    print()
+    hex_matrix_to_image(img_matrix, output_img_path)
