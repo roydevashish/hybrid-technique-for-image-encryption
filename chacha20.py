7ce86ce7c5182ffcddb136de0267813f8ff3c2a2
Devashish Roy <medevashishroy@gmail.com>
2024-12-24 01:04:02 +0530

    code restructured

--- chacha20.py
+++ chacha20.py
@@ -11,7 +11,6 @@ def encryption(plaintext, key):
     
     return ciphertext.hex(), nonce.hex()
 
-
 def decryption(ciphertext, key, nonce):
     ciphertext = unhexlify(ciphertext)
     key = unhexlify(key)
@@ -21,50 +20,3 @@ def decryption(ciphertext, key, nonce):
     plaintext = cipher.decrypt(ciphertext)
     
     return plaintext.hex()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def decryption(ciphertext, key):
-#     plaintext = ciphertext + key
-#     return plaintext
-
-
-# import json
-# from base64 import b64encode
-# from Crypto.Cipher import ChaCha20
-# from Crypto.Random import get_random_bytes
-
-# plaintext = b'\x00\x00\x00'
-# key = get_random_bytes(32)
-# cipher = ChaCha20.new(key=key)
-# ciphertext = cipher.encrypt(plaintext)
-
-# # nonce = b64encode(cipher.nonce).decode('utf-8')
-# # ct = b64encode(ciphertext).decode('utf-8')
-# # result = json.dumps({'nonce':nonce, 'ciphertext':ct})
-# print(ciphertext.hex())
-
-# output_file = open("output.txt", "w")
-# output_file.write(str(plaintext.hex()) + "\n")
-# output_file.write(str(key.hex()) + "\n")
-# output_file.write(str(ciphertext.hex()) + "\n")
