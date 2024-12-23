908a05b1669add98715790364aa7301522f471bb
Devashish Roy <medevashishroy@gmail.com>
2024-12-23 12:58:27 +0530

    modified
    Modified to handle multiple files in the input dir.

--- test.py
+++ test.py
@@ -2,12 +2,7 @@ from imgtohex import image_to_hex_matrix
 from hextoimg import hex_matrix_to_image
 from chacha20 import encryption, decryption
 
-from os import urandom
-
-if __name__ == "__main__":
-    input_img_path = "input/sample-01.jpg"
-    output_img_path = "output/sample-01.jpg"
-
+def encryption_wrapper(input_img_path, output_img_path):
     img_matrix = image_to_hex_matrix(input_img_path)
     key = "f81d2e2288bd230adaaab9e6c84aa7689550fe3d271df579bd9bb6e8a379e1d1"
 
